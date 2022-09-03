e7ebbec7094fbd34511d31513cd6cd8a0f93de6c
stenio-lima <steniolima91@outlook.com>
2022-09-03 20:42:52 -0300

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Stênio Lima on 03/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
